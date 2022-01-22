5019fc6046f39a8cfea93d0dd4c608edf4f62f3a
gkehgl1 <gkehgl2@naver.com>
2022-01-22 17:01:10 +0900

    create react app, set front view

---
+++ dhportfolio/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <body>
+        <div>
+          <section>
+            <h2>안녕하세요</h2>
+          </section>
+        </div>
+        
+      </body>
+    </div>
+    
+  );
+}
+
+export default App;
